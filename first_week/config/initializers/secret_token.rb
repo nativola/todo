60c82de43778fdb9a4c72438d4ad943484ac1f50
jasonmc02 <jasonmc02@gmail.com>
2012-11-13 13:30:28 -0600

    pjt

---
+++ first_week/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstWeek::Application.config.secret_token = '198a98a6ef9f1853254b6ed92705bfab6a05bc4ccce7a33719c64d99c6718f4a0ee26f5e5711e950b62badacc2190d22296471aceb9659fdb952c1eff8f181c4'
